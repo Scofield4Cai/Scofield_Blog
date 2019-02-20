e75d2716ee6366a0548c4044fac493c59c498a0e
Cai songyan <caisongyan@sztozed.net>
2019-02-20 11:44:02 +0800

    modify conf.py

--- source/conf.py
+++ source/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = []
 
 html_theme = 'default'
 
-html_static_path = ['_static']
+html_static_path = ['scofieldstatic']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Python-Time BLOG'
